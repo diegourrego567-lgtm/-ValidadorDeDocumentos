205938536764996a57cf1eb12110d6ae6c06fa20
diegourrego567-lgtm <diegourrego567@gmail.com>
2026-02-17 02:12:25 +0000

    new

---
+++ NormalizadorID.java
@@ -0,0 +1,31 @@
+public class NormalizadorID {
+    
+    public static void main(String[] args) {
+        NormalizadorID normalizador = new NormalizadorID();
+        normalizador.procesarID();
+    }
+
+
+    public void procesarID() {
+
+     
+        String idSucio = " Usuario_123  ";
+         String idLimpio = idSucio.trim().toUpperCase().substring(0, 8);
+
+        System.out.println("id original: " + idSucio);
+        System.out.println("id limpio: " + idLimpio);
+
+        System.out.println("\noriginal: " + System.identityHashCode(idSucio));
+        System.out.println(" limpio: " + System.identityHashCode(idLimpio));
+
+            StringBuilder builder = new StringBuilder(idLimpio);
+
+            builder.reverse();
+
+        String idInvertido = builder.toString();
+
+        System.out.println("\nID invertido: " + idInvertido);
+      
+    
+  }
+}
